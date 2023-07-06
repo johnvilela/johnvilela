b0ffd831f45aa3293826119f0d61afe049ba44ad
John Vilela <johnvvilela@gmail.com>
2023-07-06 19:13:40 -0300

    feat: improving home design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { VT323 } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react'
+import Link from 'next/link'
 const mainFont = VT323({ subsets: ['latin'], weight: ['400'] })
 
 export const metadata = {
@@ -16,7 +17,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={mainFont.className}>
-        {children}
+        <main className="w-screen h-screen flex flex-col p-4 bg-stone-950 text-custom-green">
+          <nav className="flex items-center justify-center">
+            <Link href="/" className=" text-3xl py-2 px-4 bg-custom-green text-stone-950 tracking-tighter absolute top-4 left-4">
+              JV
+            </Link>
+            <Link href="/about" className="p-4 hover:bg-custom-green hover:text-stone-950 text-xl">
+              About
+            </Link>
+            <Link href="/projects" className="p-4 hover:bg-custom-green hover:text-stone-950 text-xl">
+              Projects
+            </Link>
+            <Link href="/contact" className="p-4 hover:bg-custom-green hover:text-stone-950 text-xl">
+              Contact
+            </Link>
+          </nav>
+          {children}
+        </main>
+
         <Analytics />
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
-    <main className="w-screen h-screen grid place-items-center bg-stone-950 text-custom-green">
-      <div className="text-center">
+    <div className="text-center grow grid place-items-center">
+      <div>
         <h1 className="text-6xl">John Vilela</h1>
         <p className="text-xl">Senior Frontend Developer</p>
       </div>
-    </main>
+    </div>
   )
 }
