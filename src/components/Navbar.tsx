b03a5589e92faf3f2516892ae6b1a334e340f522
John Vilela <johnvvilela@gmail.com>
2023-07-06 22:01:12 -0300

    feat: improve mobile navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,13 +6,13 @@ import { usePathname } from 'next/navigation';
 export function Navbar() {
   const currentRoute = usePathname();
 
-  const defaultStyle = "p-4 text-xl hover:bg-custom-green hover:text-stone-950";
+  const defaultStyle = "p-2 lg:p-4 lg:text-xl hover:bg-custom-green hover:text-stone-950";
   const activeStyle = `${defaultStyle} bg-custom-green text-stone-950`;
   const nonActiveStyle = `${defaultStyle} bg-stone-950 text-custom-green`;
 
   return (
-    <nav className="flex items-center justify-center">
-      <Link href="/" className=" text-3xl py-2 px-4 bg-custom-green text-stone-950 tracking-tighter absolute top-4 left-4">
+    <nav className="flex items-center justify-end lg:justify-center">
+      <Link href="/" className="lg:text-3xl text-xl py-2 px-4 bg-custom-green text-stone-950 tracking-tighter absolute top-4 left-4">
         JV
       </Link>
       <Link href="/about" className={currentRoute === "/about" ? activeStyle : nonActiveStyle}>
